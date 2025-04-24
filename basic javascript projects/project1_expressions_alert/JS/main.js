d4656032dba5f0cf3f4bee902f863edea966d887
edendior <edendior1@gmail.com>
2025-04-24 00:53:21 -0700

    completed JS assignement

---
+++ basic javascript projects/project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+// displays: Hello, World! in an alert box
+window.alert("Hello, World!"); 
+
+// declaring the two string variables
+var sent1 = "This is the beginning of the string ";
+var sent2 = "and this is the end of the string.";
+
+// concatenating the strings together in a new variable
+var fullSentence = sent1 + sent2;
+
+// displaying the result which is the full sentence on the page
+document.write(fullSentence + "<br>");
+
+// a simple expression 
+document.write("3 + 3 = " + (3 + 3)); // displays: 3 + 3 = 6
