2fc9f5accddf440cfbac605642f1e89561de5026
edendior <edendior1@gmail.com>
2025-04-25 14:35:59 -0700

    Delete project3_math_operators directory to add correct file

--- project3_math_operators/JS/main.js
+++
@@ -1,5 +0,0 @@
-function addition_Function() { //creates a function named addition_Function
-    var addition = 2 + 2; //telling JS that addition is equal to 2 + 2
-    document.getElementById("Math").innerHTML = "2 + 2 = " + addition; //when "Math" is called in 
-    //HTML, it will display the string "2 + 2 = " and the value of addition
-}
